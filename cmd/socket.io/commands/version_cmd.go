63bf81ec3540ee7f6d971eb7b175a61c0b35e233
luoyy <zishang520@gmail.com>
2025-04-21 21:17:40 +0800

    refactor: remove module update logic from version command

--- cmd/socket.io/commands/version_cmd.go
+++ cmd/socket.io/commands/version_cmd.go
@@ -5,7 +5,6 @@ import (
 	"os"
 	"os/exec"
 	"regexp"
-	"strings"
 
 	"github.com/symfony-cli/console"
 	"github.com/symfony-cli/terminal"
@@ -53,41 +52,6 @@ func versionCommand() *console.Command {
 			updated := regexp.MustCompile(`VERSION\s*=\s*".*?"`).ReplaceAll(content, []byte(fmt.Sprintf(`VERSION = "%s"`, version)))
 			if err := os.WriteFile(versionFile, updated, 0644); err != nil {
 				return fmt.Errorf("failed to write %s: %w", versionFile, err)
-			}
-
-			// Step 2: Update modules
-			modules := []string{`parsers/engine`, `parsers/socket`, `servers/engine`, `servers/socket`, `adapters/adapter`, `adapters/redis`, `clients/engine`, `clients/socket`}
-			for _, mod := range modules {
-				if _, err := os.Stat(mod); os.IsNotExist(err) {
-					terminal.Printf("[Warn] Skipped missing module: %s\n", mod)
-					continue
-				}
-
-				terminal.Printf("[Version] Updating dependencies in %s...\n", mod)
-				if err := runInDir(mod, func() error {
-					if err := run("go", "mod", "tidy"); err != nil {
-						return err
-					}
-					// Filter and update specific dependencies
-					cmd := exec.Command("go", "list", "-f", "{{if and (not .Indirect) (not .Main)}}{{.Path}}@"+version+"{{end}}", "-m", "all")
-					cmd.Env = os.Environ()
-					cmd.Dir = mod
-					output, err := cmd.Output()
-					if err != nil {
-						return err
-					}
-					lines := strings.Split(string(output), "\n")
-					for _, line := range lines {
-						if strings.HasPrefix(line, "github.com/zishang520/socket.io") {
-							if err := run("go", "get", "-v", line); err != nil {
-								return err
-							}
-						}
-					}
-					return run("go", "mod", "tidy")
-				}); err != nil {
-					return fmt.Errorf("error in module %s: %w", mod, err)
-				}
 			}
 
 			terminal.Println("[Version] Done.")
