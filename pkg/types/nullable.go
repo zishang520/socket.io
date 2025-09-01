9b31d2e1184e68ba68bc6396bcfd67e0813b127d
luoyy <zishang520@gmail.com>
2025-09-01 10:55:47 +0800

    refactor(types): add IsPresent and IsEmpty methods to Optional interface and handle nil case in Some.Get

--- pkg/types/nullable.go
+++ pkg/types/nullable.go
@@ -1,6 +1,8 @@
 package types
 
 type Optional[T any] interface {
+	IsPresent() bool
+	IsEmpty() bool
 	Get() T
 }
 
@@ -12,6 +14,16 @@ func NewSome[T any](value T) Optional[T] {
 	return &Some[T]{value: value}
 }
 
+// IsPresent returns true if this Some is not nil
+func (s *Some[T]) IsPresent() bool { return s != nil }
+
+// IsEmpty returns true if this Some is nil
+func (s *Some[T]) IsEmpty() bool { return s == nil }
+
 func (s *Some[T]) Get() T {
+	if s == nil {
+		var zero T
+		return zero
+	}
 	return s.value
 }
